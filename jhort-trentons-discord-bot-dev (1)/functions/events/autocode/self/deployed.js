a7c10259aaf2d9a4ce2029ff526f9f9b603eaa5e
JoshHorton2020 <73555787+JoshHorton2020@users.noreply.github.com>
2023-09-17 14:36:26 -0500

    Update deployed.js

--- jhort-trentons-discord-bot-dev (1)/functions/events/autocode/self/deployed.js
+++ jhort-trentons-discord-bot-dev (1)/functions/events/autocode/self/deployed.js
@@ -12,17 +12,3 @@ let channels = await lib.discord.guilds['@0.0.6'].channels.list({
 let generalChannel = channels.find((channel) => {
   return channel.name === 'general';
 });
-
-if (generalChannel) {
-  let botInfo = await lib.discord.users['@0.0.6'].me.list();
-  await lib.discord.channels['@0.0.6'].messages.create({
-    channel_id: generalChannel.id,
-    content: [
-      `Hey there! Thanks for installing this Autocode-powered Discord bot.`,
-      `Try mentioning me in a message like this: <@!${botInfo.id}>`,
-      '',
-      `You can edit the code for the bot from your Autocode account, or read more about the app here:`,
-      'https://autocode.com/app/discord/basic-discord-example/'
-    ].join('\n')
-  });
-}
